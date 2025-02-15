6f05462f91b5075b7073ce934ba26e1efdf7bdba
wwaaafa <153055137+wwaaafa@users.noreply.github.com>
2025-02-15 10:38:30 +0800

    load

---
+++ omni_sl8541e_1h10.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from sl8541e_1h10 device
+$(call inherit-product, device/jy001-watch/sl8541e_1h10/device.mk)
+
+PRODUCT_DEVICE := sl8541e_1h10
+PRODUCT_NAME := omni_sl8541e_1h10
+PRODUCT_BRAND := SPRD
+PRODUCT_MODEL := JY001-Watch 520
+PRODUCT_MANUFACTURER := jy001-watch
+
+PRODUCT_GMS_CLIENTID_BASE := android-jy001-watch
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="sl8541e_1h10_oversea-user 8.1.0 OPM2.171019.012 11312 release-keys"
+
+BUILD_FINGERPRINT := SPRD/sl8541e/sl8541e:8.1.0/OPM2.171019.012/11312:user/release-keys
